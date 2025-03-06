d7c5355669ce26c6f1dad1ab0ec7b0767b94a67c
RarastW <ariqnayaka08@gmail.com>
2025-03-06 19:03:15 +0700

    minggu 4
    tugas

---
+++ 4/script.js
@@ -0,0 +1,57 @@
+let notes = [];
+
+function loadData() {
+  const data = localStorage.getItem("notes");
+  if (data) {
+    notes = JSON.parse(data);
+  }
+}
+
+function saveData() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+let notesElement = document.getElementById("notes-list");
+// console.log(notesElement);
+
+function renderNotes() {
+  notesElement.innerHTML = "";
+  notes.forEach((note, index) => {
+    const newElement = `
+      <div style="width: max-content; height: 10rem; background-color: white; border: 1px solid gray; display: flex; flex-direction: column; padding: 1rem 1rem 1rem 1rem; justify-content: space-between; border-radius: 5px">
+            <h2>${note.title}</h2>
+            <p style="color: rgb(93, 92, 92)">${note.description}</p>
+            <p style="font-size: small; color: gray">Deadline : ${note.deadline}</p>
+            <button onclick="deleteNote(${index})" style="background-color: orangered; width: max-content; padding: 0.5rem 1rem 0.5rem 1rem; border-radius: 5px; border: 1px solid gray">Hapus</button>
+          </div>
+      `;
+
+    notesElement.innerHTML += newElement;
+  });
+}
+
+function addNote() {
+  const newTitle = document.getElementById("namaTugas").value;
+  const newDesc = document.getElementById("deskripsiTugas").value;
+  const newDead = document.getElementById("tanggalTugas").value;
+  console.log(newDesc);
+  if (newTitle && newDesc && newDead && newTitle.length > 1 && newDesc.length > 1 && newDead.length > 1) {
+    const newData = {
+      title: newTitle,
+      description: newDesc,
+      deadline: newDead,
+    };
+
+    notes.push(newData);
+  }
+  saveData();
+  renderNotes();
+}
+
+function deleteNote(index) {
+  notes.splice(index, 1);
+  renderNotes();
+}
+
+loadData();
+renderNotes();
